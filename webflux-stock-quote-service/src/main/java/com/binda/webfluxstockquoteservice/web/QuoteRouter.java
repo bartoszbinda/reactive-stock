3c86f3e34d06a87cad8c9efd4a1662fbb19975f4
Bartosz Binda <batinex@gmail.com>
2017-12-20 11:39:04 +0100

    rewritten quoteRouter

--- webflux-stock-quote-service/src/main/java/com/binda/webfluxstockquoteservice/web/QuoteRouter.java
+++ webflux-stock-quote-service/src/main/java/com/binda/webfluxstockquoteservice/web/QuoteRouter.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
