83f70aa30c5b6dc1a1ef7a8aa403c328cc919f13
Bartosz Binda <batinex@gmail.com>
2017-12-20 11:41:42 +0100

    created reactive quote service

---
+++ mongodb-reactive-stock-service/src/main/java/com/binda/mongodbreactivestockservice/MongodbReactiveStockServiceApplication.java
@@ -0,0 +1,12 @@
+package com.binda.mongodbreactivestockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbReactiveStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbReactiveStockServiceApplication.class, args);
+	}
+}
---
+++ mongodb-reactive-stock-service/src/test/java/com/binda/mongodbreactivestockservice/MongodbReactiveStockServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.binda.mongodbreactivestockservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MongodbReactiveStockServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
